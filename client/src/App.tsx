d1e34d44c00f8cc61c743e4c1078d13a32141a22
SOTA <sota00@outlook.jp>
2022-02-06 10:54:49 -0800

    add header and footer with styles #11

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,19 @@
 import { FC } from "react";
 import { Routes, Route } from "react-router-dom";
 
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import HomeScreen from "./pages/HomeScreen";
 
 const App: FC = () => {
   return (
-    <Routes>
-      <Route index element={<HomeScreen />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route index element={<HomeScreen />} />
+      </Routes>
+      <Footer />
+    </>
   );
 };
 
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+import { Link } from "react-router-dom";
+
+import "../css/components/header.css";
+
+const Header: FC = () => {
+  return (
+    <div className="header__container">
+      <div className="header__element--laptop" />
+      <Link to="/">
+        <img src="/logo.png" alt="logo" className="header__logo" />
+      </Link>
+      <div className="header__wrap--right">
+        <Link to="/explore" className="header__link--explore">
+          Explore
+        </Link>
+        <Link to="/user">
+          <img src="/user.svg" alt="user" />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
